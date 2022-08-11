23163a01816e26749d032b35e8ee30cf0b1c6336
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-08-11 17:31:10 -0400

    small adjustments in javascript files

--- models/Comment.js
+++ models/Comment.js
@@ -4,43 +4,43 @@ const sequelize = require('../config/connection');
 class Comment extends Model {}
 
 Comment.init(
-    {
-      id: {
-          type: DataTypes.INTEGER,
-          primaryKey: true,
-          allowNull: false,
-          autoIncrement: true
-      },
-      comment_text: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-              len:[1]
-          }
-      },
-      user_id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          references: {
-              model: 'user',
-              key: 'id'
-              }
-      },
-      post_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-          model: 'post',
-          key: 'id'
-          }
-      }
+  {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
     },
-    {
-      sequelize,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'comment'
+    comment_text: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len:[1]
+        }
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id'
+            }
+    },
+    post_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+        model: 'post',
+        key: 'id'
+        }
     }
-  );
-  
-  module.exports = Comment;
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+  }
+);
+
+module.exports = Comment;
--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+// create our Post model
 class Post extends Model {}
 
 // create fields/columns for Post model
@@ -16,15 +17,15 @@ Post.init(
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-            len: [1]
-        }
+          len:[1]
+      }
       },
       post_text: {
         type: DataTypes.TEXT,
         allowNull: false,
         validate: {
           len:[1]
-        }
+      }
       },
       user_id: {
         type: DataTypes.INTEGER,
