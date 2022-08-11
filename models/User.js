b1b9bb8435172bb65c6bfd8e10d7d19d93aaee5f
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-08-11 09:33:13 -0400

    created login route and user verification

--- models/User.js
+++ models/User.js
@@ -3,7 +3,12 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 //create our User model
-class User extends Model {}
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 //deifne table columns and configuration
 
@@ -28,7 +33,15 @@ User.init (
             validate: {
                 len: [4]
             }
-        }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
 
     },
     {
