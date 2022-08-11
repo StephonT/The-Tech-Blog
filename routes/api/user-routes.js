7e370804aa7592d0c882d7aecaaa0a114b4b9ad3
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-08-11 09:06:27 -0400

    hashed password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,7 @@ const { User } = require('../../models');
 // GET /api/users
 router.get('/', (req, res) => {
     User.findAll({
-        attributes: { exclude: ['password'] }
+        // attributes: { exclude: ['password'] }
     })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
@@ -50,7 +50,8 @@ router.post('/', (req, res) => {
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
-    User.update({
+    User.update(req.body, {
+        individualHooks: true,
       where: {
         id: req.params.id
       }
