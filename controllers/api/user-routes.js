3b2eb07069dc634e60a2c04595c0e9e15776c5a5
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-08-11 17:37:19 -0400

    user-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -52,47 +52,46 @@ router.get('/:id', (req, res) => {
 });
 
 
-// POST /api/users
+// CREATE new user 
 router.post('/', (req, res) => {
     User.create({
         username: req.body.username,
         email: req.body.email,
         password: req.body.password
     })
-    //store user data during session
+    // store user data during session 
     .then(dbUserData => {
-        req.session.save(() => {
-            req.session.user_id = dbUserData.id;
-            req.session.username = dbUserData.username;
-            req.session.loggedIn = true;
-    
-            res.json(dbUserData);
-            });
+    req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
         });
     });
+});
 
+// LOG IN for users/ verify users 
 router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
     User.findOne({
         where: {
-          email: req.body.email
+            email: req.body.email
         }
-      }).then(dbUserData => {
+    }).then(dbUserData => {
         if (!dbUserData) {
-          res.status(400).json({ message: 'No user with that email address!' });
-          return;
+            res.status(400).json({ message: 'No user with that email address!'});
+            return;
         }
-    
-        // res.json({ user: dbUserData });
-    
-        // Verify user
+        // res.json({ user: dbUserData});
+        // verify user
         const validPassword = dbUserData.checkPassword(req.body.password);
 
         if (!validPassword) {
             res.status(400).json({ message: 'Incorrect password!' });
             return;
-          }
-          
-          req.session.save(() => {
+        }
+        req.session.save(() => {
             // declare session variables
             req.session.user_id = dbUserData.id;
             req.session.username = dbUserData.username;
@@ -102,7 +101,9 @@ router.post('/login', (req, res) => {
         });
     });
 });
-    // LOG OUT 
+    
+
+// LOG OUT 
 router.post('/logout', withAuth, (req, res) => {
     if (req.session.loggedIn) {
         req.session.destroy(() => {
@@ -113,45 +114,45 @@ router.post('/logout', withAuth, (req, res) => {
     }
 });
 
-// PUT /api/users/1
+// UPDATE a user 
 router.put('/:id', withAuth, (req, res) => {
     User.update(req.body, {
         individualHooks: true,
-      where: {
-        id: req.params.id
-      }
+        where: {
+            id: req.params.id
+        }
     })
     .then(dbUserData => {
         if (!dbUserData[0]) {
-            res.status(404).json({ message: 'No user found at this id' });
+            res.status(404).json({ message: 'No user found with this id'});
             return;
         }
         res.json(dbUserData);
     })
     .catch(err => {
-        console.log(err);
+        console.log(err); 
         res.status(500).json(err);
-    })
+    });
 });
 
-// DELETE /api/users/1
+// DELETE a user
 router.delete('/:id', withAuth, (req, res) => {
     User.destroy({
         where: {
             id: req.params.id
         }
     })
-    .then(dbUserData => {
-        if (!dbUserData) {
-            res.status(404).json({ message: 'No user found with this id' })
-            return;
-        }
-        res.json(dbUserData)
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
